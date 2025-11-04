728d8cc79a6a4db595cfb956267a064d1647dbee
rahulpemmaraju <prahul1025@gmail.com>
2025-11-04 15:18:46 -0500

    added readme and requirements

---
+++ README.md
@@ -0,0 +1,10 @@
+# Brain Inspired Computing Final Project: SNNs for Automatic Arrhythmia Detection
+
+## Installation
+
+Create a virtual environment (recommended) and install all project dependencies.
+
+```
+pip install -r requirements.txt
+```
+
--- models/custom_snn.py
+++ models/custom_snn.py
@@ -65,6 +65,17 @@ class SNN_Cell(nn.Module):
 
         return v_out, z_out
     
+    def forward_hebbian(self, x_in, v_in, z_in):
+        with torch.no_grad():
+            v_out = self.beta * v_in + self.z_v.forward_hebbian(z_in) + self.x_v.forward_hebbian(x_in) - (z_in * self.threshold).detach()
+            z_out = self.spike_grad(v_out - self.threshold)
+
+        return v_out, z_out
+    
+    def apply_weight_change(self):
+        self.z_v.apply_weight_change()
+        self.x_v.apply_weight_change()
+    
 # Basic 2-Layer SNN Using LIF Neurons
 class TwoLayerSNN(nn.Module):
 
@@ -108,10 +119,8 @@ class TwoLayerSNN(nn.Module):
         self.snn_1 = SNN_Cell(self.in_dims, self.recurrent_dims[0], **neuron_options)
         self.snn_2 = SNN_Cell(self.recurrent_dims[0], self.recurrent_dims[1], **neuron_options)
 
-        self.fc = nn.Sequential(
-            HebbianLinearLayer(self.recurrent_dims[1], self.out_dims, **neuron_options['linear_options']),
-            get_activation_fn(out_act, out_act_kwargs)
-        )
+        self.fc = HebbianLinearLayer(self.recurrent_dims[1], self.out_dims, **neuron_options['linear_options'])
+        self.fc_act = get_activation_fn(out_act, out_act_kwargs)
 
     def forward(self, x):
         # x shape: B x T x IN_DIMS
@@ -137,89 +146,52 @@ class TwoLayerSNN(nn.Module):
         if self.accumulate == 'sum':
             out_spikes = torch.stack(out_spikes).transpose(0, 1).mean(1)
             output = self.fc(out_spikes)
+            output = self.fc_act(output)
         else:
             output = self.fc(z_1)
+            output = self.fc_act(output)
 
         return output
     
-# 2-Layer Heirarchical SNN Using LIF Neurons
-class TwoLayerSNN(nn.Module):
-
-    def __init__(
-            self,
-            in_dims: int,
-            out_dims: int,
-            recurrent_dims: List[int],
-            out_act: str = 'none',
-            out_act_kwargs: dict = {},
-            neuron_options: dict = {
-                'beta': 0.9,
-                'threshold': 1.0,
-                'spike_grad': surrogate.atan(alpha=2), 
-                'linear_options':{
-                    'learning_rule': 'oja',
-                    'learning_rate': 1e-4,
-                    'bias': True
-                }
-            },
-            spike_accumulator: str = 'sum',
-        ):
-
-        '''
-        in_dims: input dimensions
-        out_dims: output dimensions
-        recurrent_dimensions: list of length two with output dimensions of recurrent layers
-        out_act: activation fn for output layer
-        out_act_kwargs: kwargs to pass to output layer
-        neuron_options: kwargs to pass to SNN_Cell
-        spike_accumulator: either "sum" or "last" -> how to pass spikes to prediction layer
-        '''
-
-        super(TwoLayerSNN, self).__init__()
-
-        self.in_dims = in_dims
-        self.out_dims = out_dims
-        self.recurrent_dims = recurrent_dims
-        self.accumulate = spike_accumulator
-
-        self.snn_1 = SNN_Cell(self.in_dims, self.recurrent_dims[0], **neuron_options)
-        self.snn_2 = SNN_Cell(self.recurrent_dims[0], self.recurrent_dims[1], **neuron_options)
-
-        self.fc = nn.Sequential(
-            HebbianLinearLayer(self.recurrent_dims[1], self.out_dims, **neuron_options['linear_options']),
-            get_activation_fn(out_act, out_act_kwargs)
-        )
-
-    def forward(self, x):
+    def forward_hebbian(self, x):
         # x shape: B x T x IN_DIMS
 
-        B, T, _ = x.shape
+        with torch.no_grad():
+            B, T, _ = x.shape
 
-        v_0 = torch.zeros(B, self.recurrent_dims[0])
-        z_0 = torch.zeros(B, self.recurrent_dims[0])
+            v_0 = torch.zeros(B, self.recurrent_dims[0])
+            z_0 = torch.zeros(B, self.recurrent_dims[0])
 
-        v_1 = torch.zeros(B, self.recurrent_dims[1])
-        z_1 = torch.zeros(B, self.recurrent_dims[1])
+            v_1 = torch.zeros(B, self.recurrent_dims[1])
+            z_1 = torch.zeros(B, self.recurrent_dims[1])
 
-        out_spikes = []
+            out_spikes = []
 
-        for t in range(T):
-            x_t = x[:, t, :]
+            for t in range(T):
+                x_t = x[:, t, :]
 
-            v_0, z_0 = self.snn_1(x_t, v_0, z_0) # output spikes are inputs to next layer
-            v_1, z_1 = self.snn_2(z_0, v_1, z_1)
+                v_0, z_0 = self.snn_1.forward_hebbian(x_t, v_0, z_0) # output spikes are inputs to next layer
+                v_1, z_1 = self.snn_2.forward_hebbian(z_0, v_1, z_1)
 
-            out_spikes.append(z_1)
+                out_spikes.append(z_1)
+            
+            if self.accumulate == 'sum':
+                out_spikes = torch.stack(out_spikes).transpose(0, 1).mean(1)
+                output = self.fc.forward_hebbian(out_spikes)
+                output = self.fc_act(output)
+            else:
+                output = self.fc.forward_hebbian(z_1)
+                output = self.fc_act(output)
+
+            return output
         
-        if self.accumulate == 'sum':
-            out_spikes = torch.stack(out_spikes).transpose(0, 1).mean(1)
-            output = self.fc(out_spikes)
-        else:
-            output = self.fc(z_1)
+    def apply_weight_change(self):
+        self.snn_1.apply_weight_change()
+        self.snn_2.apply_weight_change()
+        self.fc.apply_weight_change()
 
-        return output
     
-# 3-Layer Heirarchical SNN Using LIF Neurons
+# 3-Layer SNN Using LIF Neurons
 class ThreeLayerSNN(nn.Module):
 
     def __init__(
@@ -263,10 +235,8 @@ class ThreeLayerSNN(nn.Module):
         self.snn_2 = SNN_Cell(self.recurrent_dims[0], self.recurrent_dims[1], **neuron_options)
         self.snn_3 = SNN_Cell(self.recurrent_dims[1], self.recurrent_dims[2], **neuron_options)
 
-        self.fc = nn.Sequential(
-            HebbianLinearLayer(self.recurrent_dims[2], self.out_dims, **neuron_options['linear_options']),
-            get_activation_fn(out_act, out_act_kwargs)
-        )
+        self.fc = HebbianLinearLayer(self.recurrent_dims[2], self.out_dims, **neuron_options['linear_options'])
+        self.fc_act = get_activation_fn(out_act, out_act_kwargs)
 
     def forward(self, x):
         # x shape: B x T x IN_DIMS
@@ -297,10 +267,55 @@ class ThreeLayerSNN(nn.Module):
         if self.accumulate == 'sum':
             out_spikes = torch.stack(out_spikes).transpose(0, 1).mean(1)
             output = self.fc(out_spikes)
+            output = self.fc_act(output)
         else:
             output = self.fc(z_2)
+            output = self.fc_act(output)
 
         return output
+
+    def forward_hebbian(self, x):
+        # x shape: B x T x IN_DIMS
+
+        with torch.no_grad():
+
+            B, T, _ = x.shape
+
+            v_0 = torch.zeros(B, self.recurrent_dims[0])
+            z_0 = torch.zeros(B, self.recurrent_dims[0])
+
+            v_1 = torch.zeros(B, self.recurrent_dims[1])
+            z_1 = torch.zeros(B, self.recurrent_dims[1])
+
+            v_2 = torch.zeros(B, self.recurrent_dims[2])
+            z_2 = torch.zeros(B, self.recurrent_dims[2])
+
+            out_spikes = []
+
+            for t in range(T):
+                x_t = x[:, t, :]
+
+                v_0, z_0 = self.snn_1.forward_hebbian(x_t, v_0, z_0) # output spikes are inputs to next layer
+                v_1, z_1 = self.snn_2.forward_hebbian(z_0, v_1, z_1)
+                v_2, z_2 = self.snn_3.forward_hebbian(z_1, v_2, z_2)
+
+                out_spikes.append(z_2)
+            
+            if self.accumulate == 'sum':
+                out_spikes = torch.stack(out_spikes).transpose(0, 1).mean(1)
+                output = self.fc(out_spikes)
+                output = self.fc_act(output)
+            else:
+                output = self.fc(z_2)
+                output = self.fc_act(output)
+
+            return output
+        
+    def apply_weight_change(self):
+        self.snn_1.apply_weight_change()
+        self.snn_2.apply_weight_change()
+        self.snn_3.apply_weight_change()
+        self.fc.apply_weight_change()
 
     
 if __name__ == '__main__':
